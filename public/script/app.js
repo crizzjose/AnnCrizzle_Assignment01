ad7d531deb46d52b5249bd77ccbf64005ea6deda
crizzjose <133920694+crizzjose@users.noreply.github.com>
2023-06-22 16:04:21 -0500

    assignment

---
+++ public/script/app.js
@@ -0,0 +1,8 @@
+//IIFE - Immediately Invoked Function Express
+(function(){
+    function Start(){
+        console.log("App Started...");
+    }
+
+    window.addEventListener("load",Start);
+})();
